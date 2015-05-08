fb73058b9c1e7eab80a8640ba3ddfe2862e10caf
binbin <774663576@qq.com>
2015-05-08 11:55:56 +0800

    first commit

---
+++ src/com/biyanzhi/bean/Picture.java
@@ -0,0 +1,71 @@
+package com.biyanzhi.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Picture {
+	private int picture_id;
+	private int publisher_id = 0;
+	private String publish_time = "";
+	private String content = "";
+	private String publisher_name = "";
+	private String publisher_avatar = "";
+	private List<PictureImage> images = new ArrayList<PictureImage>();
+
+	public int getPicture_id() {
+		return picture_id;
+	}
+
+	public void setPicture_id(int picture_id) {
+		this.picture_id = picture_id;
+	}
+
+	public int getPublisher_id() {
+		return publisher_id;
+	}
+
+	public void setPublisher_id(int publisher_id) {
+		this.publisher_id = publisher_id;
+	}
+
+	public String getPublish_time() {
+		return publish_time;
+	}
+
+	public void setPublish_time(String publish_time) {
+		this.publish_time = publish_time;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getPublisher_name() {
+		return publisher_name;
+	}
+
+	public void setPublisher_name(String publisher_name) {
+		this.publisher_name = publisher_name;
+	}
+
+	public String getPublisher_avatar() {
+		return publisher_avatar;
+	}
+
+	public void setPublisher_avatar(String publisher_avatar) {
+		this.publisher_avatar = publisher_avatar;
+	}
+
+	public List<PictureImage> getImages() {
+		return images;
+	}
+
+	public void setImages(List<PictureImage> images) {
+		this.images = images;
+	}
+
+}
---
+++ src/com/biyanzhi/bean/PictureImage.java
@@ -0,0 +1,32 @@
+package com.biyanzhi.bean;
+
+public class PictureImage {
+	private int image_id;
+	private int picture_id;
+	private int image_url;
+
+	public int getImage_id() {
+		return image_id;
+	}
+
+	public void setImage_id(int image_id) {
+		this.image_id = image_id;
+	}
+
+	public int getPicture_id() {
+		return picture_id;
+	}
+
+	public void setPicture_id(int picture_id) {
+		this.picture_id = picture_id;
+	}
+
+	public int getImage_url() {
+		return image_url;
+	}
+
+	public void setImage_url(int image_url) {
+		this.image_url = image_url;
+	}
+
+}
