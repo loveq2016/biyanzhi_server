7ce296d2d656e3964ca28dc017cd296283589887
songbinbin <774663576@qq.com>
2015-06-17 19:22:01 +0800

    a

--- src/com/biyanzhi/util/Constants.java
+++ src/com/biyanzhi/util/Constants.java
@@ -1,7 +1,7 @@
 package com.biyanzhi.util;
 
 public class Constants {
-	public static final String SERVER_PATH = "http://192.168.1.108:8080/biyanzhi";
+	public static final String SERVER_PATH = "http://192.168.1.102:8080/biyanzhi";
 	public static final String APP_AVATAR = "http://123.56.46.254:8080/InterestFriend/images/app_icon.png";
 
 }
