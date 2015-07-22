e5f255e7e49acf2d959b0119495e6e85951e27df
binbin <774663576@qq.com>
2015-07-22 10:18:23 +0800

    a

--- src/com/biyanzhi/util/Constants.java
+++ src/com/biyanzhi/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final int APP_VSERSION_CODE = 5;
 	public static final String VERSION_INFO = "增加意见反馈功能\n优化界面显示";
 	public static final String APP_LINK = "http://123.56.46.254/biyanzhi.apk";
-	public static final String SERVER_PATH = "http://123.56.46.254:8080/biyanzhi";
+	public static final String SERVER_PATH = "http://192.168.1.108:8080/biyanzhi";
 	public static final String APP_AVATAR = "http://123.56.46.254:8080/biyanzhi/app/app_logo.png";
 
 }
